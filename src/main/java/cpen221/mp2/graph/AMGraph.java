88422fede3d290dae6614c41ed52d38455783423
NimeshPandey <nimeshpnd14@gmail.com>
2023-11-12 19:38:55 -0800

    updated

--- src/main/java/cpen221/mp2/graph/AMGraph.java
+++ src/main/java/cpen221/mp2/graph/AMGraph.java
@@ -9,57 +9,49 @@ public class AMGraph<V extends Vertex, E extends Edge<V>>
      *
      * @param maxVertices is greater than 1
      */
-    private Map<V, List<E>> adjacencyList;
-    private int maxVert;
+    private Map<V, Integer> vertices;
+    private E[][] adjacencymatrix;
+    private int maxVertices;
 
     public AMGraph(int maxVertices) {
-        this.maxVert = maxVertices;
-        this.adjacencyList = new HashMap<>(maxVert);
+        this.vertices = new HashMap<>();
+        this.maxVertices = maxVertices;
+        this.adjacencymatrix = (E[][]) new Edge[maxVertices][maxVertices];
+
     }
 
     @Override
     public boolean addVertex(V v) {
-        boolean vertexaddition;
-
-        if (adjacencyList.size() == maxVert)
-        {
+        boolean vertexaddition = false;
+        if (hasVertex(v) || vertices.size() == maxVertices) {
             return false;
+        } else {
+            int newIndex = vertices.size();
+            vertices.put(v, newIndex);
+            return true;
         }
-        else if (hasVertex(v))
-        {
-            vertexaddition = false;
+    }
+
+    @Override
+    public boolean addEdge(E e) {
+        boolean EdgeADD;
+        if (!hasEdge(e) || !hasVertex(e.v1()) || !hasVertex(e.v2())) {
+            return false;
+        } else {
+            V v1 = e.v1();
+            V v2 = e.v2();
+            adjacencymatrix[vertices.get(v1)][vertices.get(v2)] = e;
+            adjacencymatrix[vertices.get(v2)][vertices.get(v1)] = e;
+            EdgeADD = true;
+
         }
-        else if(adjacencyList.isEmpty())
-        {
-            adjacencyList.put(v,new ArrayList<>());
-            vertexaddition = true;
-        }
-        else
-        {
-            adjacencyList.put(v, new ArrayList<>());
-            vertexaddition = true;
-        }
-        return vertexaddition;
+        return EdgeADD;
     }
 
 
     @Override
     public boolean hasVertex(V v) {
-        return adjacencyList.containsKey(v);
-    }
-
-    @Override
-    public boolean addEdge(E e) {
-        boolean EdgeADD ;
-        if (!hasEdge(e) || !hasVertex(e.v1()) || !hasVertex(e.v2())) {
-            return false;
-        } else
-        {
-            adjacencyList.get(e.v1()).add(e);
-            EdgeADD = true;
-
-        }
-        return EdgeADD;
+        return vertices.containsKey(v);
     }
 
     @Override
@@ -69,178 +61,261 @@ public class AMGraph<V extends Vertex, E extends Edge<V>>
 
     @Override
     public boolean hasEdge(V v1, V v2) {
-        for (E edge : adjacencyList.get(v1)) {
-            if (edge.v2().equals(v2)) {
-                return true;
-            }
+        if (vertices.containsKey(v1) && vertices.containsKey(v2)) {
+            return adjacencymatrix[vertices.get(v1)][vertices.get(v2)] != null || adjacencymatrix[vertices.get(v2)][vertices.get(v1)] != null;
         }
         return false;
     }
 
     @Override
     public int edgeLength(V v1, V v2) {
-        int edgelength = 0;
-        for (E edge : adjacencyList.get(v1)) {
-            if (edge.v2().equals(v2)) {
-                edgelength = edge.length();
-                return edgelength;
+        if (vertices.containsKey(v1) && vertices.containsKey(v2)) {
+            int index1 = vertices.get(v1);
+            int index2 = vertices.get(v2);
+
+            Edge<V> edge = adjacencymatrix[index1][index2];
+            if (edge != null) {
+                return edge.length();
             }
-            return 0;
+            return -1;
         }
-        return edgelength;
+        return -1;
     }
 
     @Override
-    public int edgeLengthSum() {
-        int sum = 0 ;
-        Set<E> alledges = allEdges();
-        List<E> converted = new ArrayList<>(alledges);
-        for(E edge : converted)
-        {
-            int length = edge.length();
-            sum = sum + length;
+    public int edgeLengthSum()
+    {
+        int sum = 0;
+        for (E[] row : adjacencymatrix) {
+            for (E edge : row) {
+                if (edge != null) {
+                    sum += edge.length();
+                }
+            }
         }
         return sum;
-
-
     }
+
 
     @Override
     public boolean removeEdge(E e) {
-        Set<V> allvertices = allVertices();
-        boolean removed = false;
-        for(V v : allvertices)
+        if (hasEdge(e))
         {
-            if(hasEdge(e.v1(),e.v2()))
-            {
-                adjacencyList.get(v).remove(e);
-                removed = true;
-            }
-        }
-
-        return removed;
+            V v1 = e.v1();
+            V v2 = e.v2();
+            adjacencymatrix[vertices.get(v1)][vertices.get(v2)] = null;
+            adjacencymatrix[vertices.get(v2)][vertices.get(v1)] = null;
+            return true;
+        }return false;
     }
-
     @Override
     public boolean removeEdge(V v1, V v2) {
         boolean result = false ;
         if(hasEdge(v1,v2))
         {
-            E e = (E) new Edge<>(v1,v2);
-            adjacencyList.get(v1).remove(e);
+           adjacencymatrix[vertices.get(v1)][vertices.get(v2)] = null;
+           adjacencymatrix[vertices.get(v2)][vertices.get(v1)] = null;
             result = true;
         }
         return result;
     }
 
     @Override
-    public boolean removeVertex(V v) {
-        boolean removal ;
-        if (adjacencyList.isEmpty()) {
-            return false;
-        } else if (adjacencyList.containsKey(v))
+    public boolean removeVertex(V v)
+    {
+        if(hasVertex(v))
         {
-            adjacencyList.remove(v);
-            removal = true;
+            vertices.remove(v);
+            return true;
         }
-        else {
-            removal = false;
-        }
-        return removal;
+      return false;
     }
 
 
 
     @Override
-    public E getEdge(V v1, V v2) {
-        List<E> list ;
-        if (hasEdge(v1, v2))
-        {
-            list = adjacencyList.get(v1);
-            {
-                for( int i = 0; i < list.size();i++)
-                {
-                    if (list.get(i).v2().equals(v2))
-                    {
-                        return list.get(i);
-                    }
-                }
-            }
-        }
-        return null;
+    public E getEdge(V v1, V v2)
+    {
+       if(hasEdge(v1,v2))
+       {
+           return adjacencymatrix[vertices.get(v1)][vertices.get(v2)];
+       }
+       return null;
     }
     @Override
     public Set<V> allVertices()
     {
-        return adjacencyList.keySet();
+        return vertices.keySet();
     }
 
     @Override
     public Set<E> allEdges() {
-        List<E> alledge1 = new ArrayList<>();
-        Set<V> allvertices =  allVertices();
-        for(V v : allvertices)
-        {
-           List<E> edgelist = adjacencyList.get(v);
-           for(E edge : edgelist)
-           {
-               alledge1.add(edge);
-           }
-        }
-        for(int i = 0; i < alledge1.size() - 1 ; i++)
-        {
-            for(int j = i+1 ; j < alledge1.size(); j++ )
-            {
-                if(alledge1.get(i).equals(alledge1.get(j)))
-                {
-                    alledge1.remove(alledge1.get(j));
+        Set<E> edges = new HashSet<>();
+        for (E[] row : adjacencymatrix) {
+            for (E edge : row) {
+                if (edge != null) {
+                    edges.add(edge);
                 }
             }
         }
-        Set<E> listofalledges = new HashSet<>(alledge1);
-
-        return listofalledges;
+        return edges;
     }
+
 
     @Override
-    public Set<E> allEdges(V v) {
-        List<E> list = new ArrayList<>();
-        Set<E> alledges = new HashSet<>();
-        if (hasVertex(v))
-        {
-            list = adjacencyList.get(v);
-            for (int i = 0; i < list.size(); i++) {
-                alledges.add(list.get(i));
+    public Set<E> allEdges(V v)
+    {
+        Set<E> edges = new HashSet<>();
+        if (vertices.containsKey(v)) {
+            int index = vertices.get(v);
+            for (int i = 0; i < adjacencymatrix.length; i++) {
+                if (adjacencymatrix[index][i] != null) {
+                    edges.add(adjacencymatrix[index][i]);
+                }
+            }
+
+
+            for (int i = 0; i < adjacencymatrix.length; i++) {
+                if (adjacencymatrix[i][index] != null) {
+                    edges.add(adjacencymatrix[i][index]);
+                }
             }
         }
-        return alledges;
+        return edges;
     }
+
 
     @Override
     public Map<V, E> getNeighbourhood(V v) {
-        Map<V,E> neighbourood = new HashMap<>();
-        for(E edge : adjacencyList.get(v))
-        {
-            neighbourood.put(edge.v2(),edge);
+        Map<V, E> neighbourhood = new HashMap<>();
+
+        if (vertices.containsKey(v)) {
+            int index = vertices.get(v);
+            for (int i = 0; i < adjacencymatrix.length; i++) {
+                E edge = adjacencymatrix[index][i];
+                if (edge != null) {
+                    neighbourhood.put(edge.v2(), edge);
+                }
+            }
+            for (int i = 0; i < adjacencymatrix.length; i++) {
+                E edge = adjacencymatrix[i][index];
+                if (edge != null) {
+                    neighbourhood.put(edge.v2(), edge);
+                }
+            }
         }
-        return neighbourood;
+
+        return neighbourhood;
     }
 
     @Override
     public int pathCost(List<V> path, PathCostType costType) {
-        return 0;
+        int sum = 0;
+        switch (costType) {
+            case MAX_EDGE:
+
+                break;
+            case MIN_EDGE:
+
+                break;
+            case SUM_EDGES:
+                for (int i = 1; i < path.size(); i++) {
+                    sum += edgeLength(path.get(i),path.get(i-1));
+                }
+                break;
+        }
+        return sum;
     }
+
+    private void minCostPath() {
+    }
+
+
+    private void maxCostPath() {
+    }
+
+    private List<V> sumCostPath(V source, V destination) {
+        // Initialize distance and previous vertex
+        Map<V, Integer> distance = new HashMap<>();
+        Map<V, V> previous = new HashMap<>();
+        for (V vertex : vertices.keySet()) {
+            distance.put(vertex, Integer.MAX_VALUE);
+            previous.put(vertex, null);
+        }
+        distance.put(source, 0);
+
+        // Use a priority queue to sort vertices based on distance
+        PriorityQueue<V> queue = new PriorityQueue<>(Comparator.comparingInt(distance::get));
+        queue.add(source);
+
+        while (!queue.isEmpty()) {
+            V current = queue.poll();
+
+            // If the current vertex has already calculated the shortest path, skip it
+            if (current.equals(destination)) {
+                break;
+            }
+
+            int currentIndex = vertices.get(current);
+
+            // Iterate through neighbors in the matrix
+            for (int i = 0; i < adjacencymatrix.length; i++) {
+                E edge = adjacencymatrix[currentIndex][i];
+
+                if (edge != null) {
+                    V neighbor = (currentIndex == i) ? edge.v2() : edge.v1();
+                    int newDistance = distance.get(current) + edge.length();
+
+                    // If a shorter path is found, update the distance and previous vertex
+                    if (newDistance < distance.get(neighbor)) {
+                        distance.put(neighbor, newDistance);
+                        previous.put(neighbor, current);
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+
+        // Build the shortest path
+        List<V> shortestPath = new ArrayList<>();
+        V current = destination;
+        while (previous.get(current) != null) {
+            shortestPath.add(current);
+            current = previous.get(current);
+        }
+        shortestPath.add(source);
+        Collections.reverse(shortestPath);
+
+        return shortestPath;
+    }
+
+
+
+
+
 
     @Override
     public List<V> minimumCostPath(V source, V sink, PathCostType costType) {
-        return null;
+        List<V> list = null;
+        switch (costType) {
+            case MAX_EDGE:
+                maxCostPath();
+                break;
+            case MIN_EDGE:
+                minCostPath();
+                break;
+            case SUM_EDGES:
+                list = sumCostPath(source, sink);
+                break;
+        }
+        return list;
     }
 
     @Override
     public Map<V, E> getNeighbourhood(V v, int range)
     {
         Map<V, E> neighbourhood = new HashMap<>();
-        for (V vertex : adjacencyList.keySet())
+        for (V vertex : vertices.keySet())
         {
             List<V> mincostpath = minimumCostPath(v, vertex, PathCostType.SUM_EDGES);
             int size = mincostpath.size();
@@ -255,13 +330,78 @@ public class AMGraph<V extends Vertex, E extends Edge<V>>
 
     @Override
     public int getDiameter(PathCostType costType) {
-        return 0;
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        for (V v : vertices.keySet()) {
+            int max = 0;
+
+            for (V v1 : vertices.keySet()) {
+                if (!v.equals(v1)) {
+                    List<V> path = minimumCostPath(v, v1, PathCostType.SUM_EDGES);
+                    int pathCost = pathCost(path, PathCostType.SUM_EDGES);
+
+                    if (pathCost > max) {
+                        max = pathCost;
+                    }
+                }
+            }
+            ts.add(max);
+        }
+
+        return ts.last();
     }
 
+
     @Override
+
     public V getCenter(PathCostType costType) {
-        return null;
+        TreeMap<Integer,V> ts = new TreeMap<>();
+        for (V v : vertices.keySet()) {
+            int min = 0;
+            for (V v1 : vertices.keySet()) {
+                if(!v.equals(v1)&&pathCost(minimumCostPath(v,v1,PathCostType.SUM_EDGES),PathCostType.SUM_EDGES)<min){
+                    min = pathCost(minimumCostPath(v,v1,PathCostType.SUM_EDGES),PathCostType.SUM_EDGES);
+                }
+            }
+            ts.put(min,v);
+        }
+        return ts.firstEntry().getValue();
     }
+    public List<List<V>> getAllPaths(V source, V destination)
+    {
+        List<List<V>> result = new ArrayList<>();
+        Set<V> visited = new HashSet<>();
+        List<V> path = new ArrayList<>();
+
+        dfs(source, destination, visited, path, result);
+
+        return result;
+    }
+
+    private void dfs(V currentVertex, V destination, Set<V> visited, List<V> path, List<List<V>> result) {
+        visited.add(currentVertex);
+        path.add(currentVertex);
+
+        if (currentVertex.equals(destination)) {
+            result.add(new ArrayList<>(path));
+        } else {
+            int currentIndex = vertices.get(currentVertex);
+            for (int i = 0; i < adjacencymatrix.length; i++) {
+                E edge = adjacencymatrix[currentIndex][i];
+
+                if (edge != null) {
+                    V neighborVertex = (currentIndex == i) ? edge.v2() : edge.v1();
+                    if (!visited.contains(neighborVertex)) {
+                        dfs(neighborVertex, destination, visited, path, result);
+                    }
+                }
+            }
+        }
+
+        visited.remove(currentVertex);
+        path.remove(path.size() - 1);
+    }
+
 }
 
 
